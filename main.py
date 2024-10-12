ec1b5c53b0c977059c34c6466af65566f125ca50
Ana <nvolk019@gmail.com>
2024-10-12 21:16:24 +0500

    completed the code for main

--- main.py
+++ main.py
@@ -9,3 +9,10 @@ def draw_petal(turtle, radius):
 
 my_radius = int(input("What is the radius of the flower? "))
 my_petals = int(input("How many petals do you want? "))
+bob = Turtle()
+for _ in range(my_petals):
+    draw_petal(bob, my_radius)
+    bob.left(360 / my_petals)
+    bob.hideturtle()
+    screen = Screen()
+    screen.exitonclick()
